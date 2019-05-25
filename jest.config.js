1adc77013ef49e080130b9d27df7ba389f02808f
vmk1vmk <vmk1vmk@googlemail.com>
2019-05-25 16:03:45 +0200

    Added test coverage with threshold and visible report.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,16 @@
+const COVERAGE_THRESHOLD_PERCENT = 95;
+
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    verbose: true,
+    maxConcurrency: Infinity,
+    coverageThreshold: {
+        global: {
+            branches: COVERAGE_THRESHOLD_PERCENT,
+            functions: COVERAGE_THRESHOLD_PERCENT,
+            lines: COVERAGE_THRESHOLD_PERCENT,
+            statements: COVERAGE_THRESHOLD_PERCENT
+        }
+    },
 };
