d6fd781baddffa04f6debf0b06d697959ba96f7e
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-05-28 22:51:43 +0200

    changed encoding method to escape, as to match URL found in iFrame and added additional tests

--- sparql-visualizer/graphical/graphicalVisualizer.ts
+++ sparql-visualizer/graphical/graphicalVisualizer.ts
@@ -2,43 +2,46 @@
   * // TODO: create a function which gets the querry and the vizualization type, then
   * returns a URL which can be given to iframe to visualize it
   */
+ /*  export function exampleQuerryFormatting() {
+    const sparqlQuery = `#defaultView:BubbleChart
+    SELECT ?count ?venue (SAMPLE(?venue_label_) AS ?venue_label) 
+    WITH {
+      SELECT (COUNT(?work) as ?count) ?venue WHERE {
+        ?work wdt:P50 wd:Q16733372 .
+        ?work wdt:P1433 ?venue .
+      }
+      GROUP BY ?venue
+    } AS %counts
+    WHERE {
+      INCLUDE %counts
+      ?venue rdfs:label ?long_venue_label FILTER(LANG(?long_venue_label) = 'en')
+      OPTIONAL { ?venue wdt:P1813 ?short_name . }
+      BIND(COALESCE(?short_name, ?long_venue_label) AS ?venue_label_)
+    }
+    GROUP BY ?venue ?count
+    ORDER BY DESC(?count)  `;
+    return sparqlQuery;
+  } */
 
-	
-
-  export function exampleQuerryFormatting() {
-    let endpointUrl = 'https://query.wikidata.org/sparql',
-    sparqlQuery = "#defaultView:BubbleChart\n" +
-    "SELECT ?count ?venue (SAMPLE(?venue_label_) AS ?venue_label) \n" +
-    "WITH {\n" +
-    "  SELECT (COUNT(?work) as ?count) ?venue WHERE {\n" +
-    "    ?work wdt:P50 wd:Q16733372 .\n" +
-    "    ?work wdt:P1433 ?venue .\n" +
-    "  }\n" +
-    "  GROUP BY ?venue\n" +
-    "} AS %counts\n" +
-    "WHERE {\n" +
-    "  INCLUDE %counts\n" +
-    "  ?venue rdfs:label ?long_venue_label FILTER(LANG(?long_venue_label) = 'en')\n" +
-    "  OPTIONAL { ?venue wdt:P1813 ?short_name . }\n" +
-    "  BIND(COALESCE(?short_name, ?long_venue_label) AS ?venue_label_)\n" +
-    "}\n" +
-    "GROUP BY ?venue ?count\n" +
-    "ORDER BY DESC(?count)  ";
-
-    return endpointUrl;
-  }
-
-  console.log(exampleQuerryFormatting());
-
-  export function tryEncodeUri(querry: string){
-    let res = encodeURI(querry);
+  export function tryEncodeUri(querry: string, endpoint: string){
+    //let res = encodeURI(querry);
+    //const res = endpoint  + encodeURIComponent(querry);
+    const res = endpoint  + escape(querry);
     return res;
   }
-  /** console.log(tryEncodeUri());*/
-  
 
-  export function tryEncodeURIComponent(querry: string) {
-    let res = encodeURIComponent(querry);
+
+  export function encodeQuerry(querry: string){
+    const res = escape(querry);
     return res;
   }
-  console.log(tryEncodeURIComponent(exampleQuerryFormatting()));
+
+  export function constructHtmlUrl(querry: string, endpoint: string){
+    const res = endpoint  + encodeQuerry(querry);
+    return res;
+  }
+
+  export function constructQuerry(vizualizationType:string, querry: string){
+    const res = '#defaultView:' + vizualizationType + '\n'+ querry;
+    return res;
+  }
