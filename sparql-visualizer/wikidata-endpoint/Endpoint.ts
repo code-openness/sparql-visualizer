96dd9e340b56e1f5e3726c918823268d10c327b0
vmk1vmk <vmk1vmk@googlemail.com>
2019-06-02 16:22:41 +0200

    Fixed prettier errors.

--- sparql-visualizer/wikidata-endpoint/Endpoint.ts
+++ sparql-visualizer/wikidata-endpoint/Endpoint.ts
@@ -2,12 +2,18 @@ type WikidataEndpointConfig = import('./index.types').WikidataEndpointConfig;
 
 export const LOCALHOST_IDENTIFIER: ReadonlyArray<string> = ['localhost', '127.0.0.1', '0.0.0.0'];
 
-export const DEFAULT_WIKIDATA_CONFIG: Required<WikidataEndpointConfig> = { host: 'wikidata.org', httpProtocol: 'https', port: 0 };
+export const DEFAULT_WIKIDATA_CONFIG: Required<WikidataEndpointConfig> = {
+    host: 'wikidata.org',
+    httpProtocol: 'https',
+    port: 0
+};
 
 export class WikidataEndpoint {
     private readonly configuration: Required<WikidataEndpointConfig>;
 
-    constructor(configuration: WikidataEndpointConfig = {}) { this.configuration = { ...DEFAULT_WIKIDATA_CONFIG, ...configuration }; }
+    constructor(configuration: WikidataEndpointConfig = {}) {
+        this.configuration = { ...DEFAULT_WIKIDATA_CONFIG, ...configuration };
+    }
 
     public getSPARQLQueryURL(): string {
         return this.getBaseUrl();
