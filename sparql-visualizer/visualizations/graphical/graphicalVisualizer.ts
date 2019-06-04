2a189c8e748c82786d6814a3dfa58b9151909904
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-06-04 17:35:42 +0200

    WIP

--- sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
+++ sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
@@ -5,7 +5,7 @@ export function graphicalVisualizerUrlConstructor(querry: string, endpoint: stri
    vizualizationType: VisualisationIdentifier, urlType: URLTypeIdentifier): string{
   const constructQuerry: string = '#defaultView:' + vizualizationType + '\n'+ querry;
   let res: string = '';
-  let encodedQuery = encodeURIComponent(constructQuerry);
+  let encodedQuery: string = encodeURIComponent(constructQuerry);
   if (urlType === 'SPARQL') {
      res = endpoint +  'sparql?query=' + encodedQuery;
   }
@@ -16,19 +16,19 @@ export function graphicalVisualizerUrlConstructor(querry: string, endpoint: stri
       res = endpoint  + 'embed.html#' + encodedQuery;
   } else {
      // Default: iFrame URL
-     encodedQuery = additionalSymbolEncoding(encodedQuery); //only needed for iFrame, for now
+     encodedQuery = additionalSymbolEncoding(encodedQuery); // only needed for iFrame, for now
      res = endpoint + 'embed.html#' + encodedQuery;
   }
   return res;
 }
 
 export function additionalSymbolEncoding (querry: string): string {
-   let replace = /\(/gi;
-   let result = querry.replace(replace, "%28");
-   let replace2 = /\)/gi;
-   let result2 = result.replace(replace2, "%29");
-   let replace3 = /\'/gi;
-   let result3 = result2.replace(replace3, "%27");
+   const replace: RegExp = /\(/gi;
+   const result: string  = querry.replace(replace, "%28");
+   const replace2: RegExp = /\)/gi;
+   const result2: string  = result.replace(replace2, "%29");
+   const replace3: RegExp = /\'/gi;
+   const result3: string  = result2.replace(replace3, "%27");
 
    return result3;
 }
