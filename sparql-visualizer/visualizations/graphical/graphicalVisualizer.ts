70851dd42576aaf960d18ba03601f881f13dee3a
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-06-04 20:58:41 +0200

    Querry string normalizer function added, removes # comments from beginning of querry if they exist as not needed for functioning URL to Wikidata

--- sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
+++ sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
@@ -3,7 +3,8 @@ type URLTypeIdentifier = import ('./index.types').URLTypeIdentifier;
 
 export function graphicalVisualizerUrlConstructor(querry: string, endpoint: string,
    vizualizationType: VisualisationIdentifier, urlType: URLTypeIdentifier): string{
-  const constructQuerry: string = `#defaultView:${vizualizationType}\n${querry}`;
+  const formattedQuerry = queryNormalizer(querry);
+  const constructQuerry: string = `#defaultView:${vizualizationType}\n${formattedQuerry}`;
   let res: string = '';
   let encodedQuery: string = encodeURIComponent(constructQuerry);
   if (urlType === 'SPARQL') {
@@ -30,4 +31,16 @@ export function additionalSymbolEncoding (querry: string): string {
    const replace3: RegExp = /\'/gi;
    const result3: string  = result2.replace(replace3, "%27");
    return result3;
+}
+
+export function queryNormalizer (query: string): string {
+   const str: string = query.trim();
+   let removeDefaultViewomment  = str.replace(/\A*(\s+)*(#defaultView).*\n?/m, '');
+   let removeInspiredByomment  = removeDefaultViewomment.replace(/\A*(\s+)*(# Inspired).*\n?/m, '');
+   let removeNameComment = removeInspiredByomment.replace(/\A*(\s+)*(# Shubhanshu).*\n?/m, '');
+   let lineArray: string[] = removeNameComment.split(/\r?\n/);
+   let trimedArray: string[] = lineArray.map(str => str.trim());
+   let newtext = trimedArray.join('\n');
+
+   return newtext;
 }
