048a5f015b7bdf23ae046b65abf10bb79760aaf8
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-06-04 17:15:02 +0200

    Added function to encode additional symbols not covered by encodeURIComponent function

--- sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
+++ sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
@@ -5,17 +5,30 @@ export function graphicalVisualizerUrlConstructor(querry: string, endpoint: stri
    vizualizationType: VisualisationIdentifier, urlType: URLTypeIdentifier): string{
   const constructQuerry: string = '#defaultView:' + vizualizationType + '\n'+ querry;
   let res: string = '';
+  let encodedQuery = encodeURIComponent(constructQuerry);
   if (urlType === 'SPARQL') {
-     res = endpoint +  'sparql?query=' + encodeURIComponent(constructQuerry);
+     res = endpoint +  'sparql?query=' + encodedQuery;
   }
   else if (urlType === 'Wikilink') {
-      res = endpoint  + '#' +encodeURIComponent(constructQuerry);
+      res = endpoint  + '#' +encodedQuery;
   }
   else if (urlType === 'HTML'){
-      res = endpoint  + 'embed.html#' + encodeURIComponent(constructQuerry);
+      res = endpoint  + 'embed.html#' + encodedQuery;
   } else {
      // Default: iFrame URL
-     res = endpoint + 'embed.html#' + escape(constructQuerry);
+     encodedQuery = additionalSymbolEncoding(encodedQuery); //only needed for iFrame
+     res = endpoint + 'embed.html#' + encodedQuery;
   }
   return res;
+}
+
+export function additionalSymbolEncoding (querry: string): string {
+   let replace = /\(/gi;
+   let result = querry.replace(replace, "%28");
+   let replace2 = /\)/gi;
+   let result2 = result.replace(replace2, "%29");
+   let replace3 = /\'/gi;
+   let result3 = result2.replace(replace3, "%27");
+
+   return result3;
 }
