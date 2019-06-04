0bd505b575dc1a82cfd36a4c99ce0c024cbd77fa
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-06-04 18:00:17 +0200

    WIP

--- sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
+++ sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
@@ -3,21 +3,21 @@ type URLTypeIdentifier = import ('./index.types').URLTypeIdentifier;
 
 export function graphicalVisualizerUrlConstructor(querry: string, endpoint: string,
    vizualizationType: VisualisationIdentifier, urlType: URLTypeIdentifier): string{
-  const constructQuerry: string = '#defaultView:' + vizualizationType + '\n'+ querry;
+  const constructQuerry: string = `#defaultView:${vizualizationType}\n${querry}`;
   let res: string = '';
   let encodedQuery: string = encodeURIComponent(constructQuerry);
   if (urlType === 'SPARQL') {
-     res = endpoint +  'sparql?query=' + encodedQuery;
+     res = `${endpoint}sparql?query=${encodedQuery}`;
   }
   else if (urlType === 'Wikilink') {
-      res = endpoint  + '#' +encodedQuery;
+      res = `${endpoint}#${encodedQuery}`;
   }
   else if (urlType === 'HTML'){
-      res = endpoint  + 'embed.html#' + encodedQuery;
+      res = `${endpoint}embed.html#${encodedQuery}`;
   } else {
      // Default: iFrame URL
      encodedQuery = additionalSymbolEncoding(encodedQuery); // only needed for iFrame, for now
-     res = endpoint + 'embed.html#' + encodedQuery;
+     res = `${endpoint}embed.html#${encodedQuery}`;
   }
   return res;
 }
@@ -29,6 +29,5 @@ export function additionalSymbolEncoding (querry: string): string {
    const result2: string  = result.replace(replace2, "%29");
    const replace3: RegExp = /\'/gi;
    const result3: string  = result2.replace(replace3, "%27");
-
    return result3;
 }
