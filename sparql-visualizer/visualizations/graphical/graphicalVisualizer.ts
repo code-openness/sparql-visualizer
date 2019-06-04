714b72a15dec3a1a68119bd8426e58745bc533d2
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-06-04 15:36:39 +0200

    Added test suit, more URL formating, types GViz

--- sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
+++ sparql-visualizer/visualizations/graphical/graphicalVisualizer.ts
@@ -1,49 +1,20 @@
 type VisualisationIdentifier = import("./index.types").VisualisationIdentifier;
+type URLTypeIdentifier = import ('./index.types').URLTypeIdentifier;
 
-/**
-  * // TODO: create a function which gets the querry and the vizualization type, then
-  * returns a URL which can be given to iframe to visualize it
-  */
- /*  export function exampleQuerryFormatting() {
-    const sparqlQuery = `#defaultView:BubbleChart
-    SELECT ?count ?venue (SAMPLE(?venue_label_) AS ?venue_label) 
-    WITH {
-      SELECT (COUNT(?work) as ?count) ?venue WHERE {
-        ?work wdt:P50 wd:Q16733372 .
-        ?work wdt:P1433 ?venue .
-      }
-      GROUP BY ?venue
-    } AS %counts
-    WHERE {
-      INCLUDE %counts
-      ?venue rdfs:label ?long_venue_label FILTER(LANG(?long_venue_label) = 'en')
-      OPTIONAL { ?venue wdt:P1813 ?short_name . }
-      BIND(COALESCE(?short_name, ?long_venue_label) AS ?venue_label_)
-    }
-    GROUP BY ?venue ?count
-    ORDER BY DESC(?count)  `;
-    return sparqlQuery;
-  } */
-
-  export function tryEncodeUri(querry: string, endpoint: string){
-    //let res = encodeURI(querry);
-    //const res = endpoint  + encodeURIComponent(querry);
-    const res = endpoint  + escape(querry);
-    return res;
+export function graphicalVisualizerUrlConstructor(querry: string, endpoint: string, vizualizationType: VisualisationIdentifier, urlType: URLTypeIdentifier){
+  const constructQuerry = '#defaultView:' + vizualizationType + '\n'+ querry;
+  let res = '';    
+  if (urlType == 'SPARQL') {
+     res = endpoint +  'sparql?query=' + encodeURIComponent(constructQuerry);
+  } 
+  else if (urlType == 'Wikilink') {
+      res = endpoint  + '#' +encodeURIComponent(constructQuerry); 
+  } 
+  else if (urlType == 'HTML'){
+      res = endpoint  + 'embed.html#' + encodeURIComponent(constructQuerry);
+  } else {
+     //Default: iFrame URL
+     res = endpoint + 'embed.html#' + escape(constructQuerry);
   }
-
-
-  export function encodeQuerry(querry: string){
-    const res = escape(querry);
-    return res;
-  }
-
-  export function constructHtmlUrl(querry: string, endpoint: string){
-    const res = endpoint  + encodeQuerry(querry);
-    return res;
-  }
-
-  export function constructQuerry(vizualizationType: VisualisationIdentifier, querry: string){
-    const res = '#defaultView:' + vizualizationType + '\n'+ querry;
-    return res;
-  }
+  return res;
+}
