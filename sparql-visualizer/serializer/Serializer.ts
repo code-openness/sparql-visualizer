8b58938566c5ee128534050d9b1a25f6c00f09ef
Celina <loepec@fu-berlin.de>
2019-06-09 15:09:16 +0200

    serializer1.1

--- sparql-visualizer/serializer/Serializer.ts
+++ sparql-visualizer/serializer/Serializer.ts
@@ -1,26 +1,56 @@
-let datas = document.querySelectorAll("[data-visualisation]");
-console.log(datas);
+// TODO import methods, identifier
 
+import { WikidataEndpoint } from '../wikidata-endpoint';
 
-let query;
-let type;
-datas.forEach(function (data) {
-    type = data.getAttribute("data-visualisation");
-    query = data.children[0].innerHTML;
-    console.log(type);
-    console.log(query);
+type VisualisationParameters = import('./index.types').VisualisationParameters;
 
-    if (type && type === "Image") {
-        // TODO Call function to create query
-        data.setAttribute("class", "embeded-responsive embeded-responsive-4by3");
-        let iframe = document.createElement('iframe');
-        iframe.setAttribute("data-source", "TEST");
-        iframe.setAttribute("class", "embeded-responsive-item");
-        data.replaceChild(iframe , data.children[0]);
-    }
-    else {
-        // TODO Call function to create tabelle
-        // TODO Tabelle
+class Serializer {
+    public iframeWrapperClass: string;
+    public iframeClass: string;
+    public dataAttributeName: string; // TODO type identifier
+
+    constructor(wrapperClass: string, classs: string, attributeName: string) {
+        this.iframeClass = classs;
+        this.iframeWrapperClass = wrapperClass;
+        this.dataAttributeName = attributeName;
     }
 
-});
+    public init(): void {
+        const dataList: NodeList = this.findAllDataElements();
+        const that = this;
+        let visElement: HTMLElement;
+        dataList.forEach(function(dataElement) {
+            visElement = that.getVisualization(dataElement as HTMLElement);
+            dataElement.appendChild(visElement);
+        });
+    }
+
+    public changeVisualization(dataElement: HTMLElement): void {
+        const visElement = this.getVisualization(dataElement);
+        dataElement.replaceChild(visElement, dataElement.children[1]);
+    }
+
+    private findAllDataElements(): NodeList {
+        const dataList: NodeList = document.querySelectorAll('[data-' + this.dataAttributeName + ']');
+        return dataList;
+    }
+
+    private getVisualization(dataElement: HTMLElement): HTMLElement {
+        const visType = dataElement.getAttribute('data-' + this.dataAttributeName);
+        const endpoint: WikidataEndpoint = dataElement.getAttribute('data-endpoint');
+        const query = dataElement.children[0].innerHTML;
+
+        let visElement: HTMLElement;
+        if (visType && visType === 'Image') {
+            // TODO of type condition
+            dataElement.setAttribute('class', this.iframeWrapperClass);
+
+            const visIdentifier = dataElement.getAttribute('data-identifier'); // TODO cast VisualisationIdentifier
+            visElement = new HTMLElement(); // TODO Call chart function to create element
+            visElement.setAttribute('class', this.iframeClass);
+        } else {
+            visElement = new HTMLElement(); // TODO Call table function to create element
+        }
+        return visElement;
+    }
+}
