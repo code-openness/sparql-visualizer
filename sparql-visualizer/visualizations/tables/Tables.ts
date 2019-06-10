1297b4b909b68f63f95ded28f0949dc122a55196
unknown <viktoria.sorgalla@fu-berlin.de>
2019-06-10 15:31:29 +0200

    Split Table.ts into Table and Serializer.

--- sparql-visualizer/visualizations/tables/Tables.ts
+++ sparql-visualizer/visualizations/tables/Tables.ts
@@ -1,4 +1,3 @@
-import { stringify } from 'querystring';
 import { SPARQLSelectResponse } from './index.types';
 
 export async function buildSPARQLquery(
@@ -21,33 +20,4 @@ export async function fetchSPARQLResponse(url: string): Promise<SPARQLSelectResp
         throw new Error(`Failed to get SPARQL Select Response: ${error}`);
     }
     return await response.json();
-}
-
-export async function buildHTMLtable(endpoint: string, sparqlQuery: string) {
-    const jsonString: any = (buildSPARQLquery(endpoint, sparqlQuery) as unknown) as string;
-    const keyValues: any = [];
-    for (let index = 0; index < jsonString.length; index++) {
-        for (const key in jsonString[index]) {
-            if (keyValues.indexOf(key) == -1) {
-                keyValues.push(key);
-            }
-        }
-    }
-
-    const table = document.createElement('table');
-
-    let tableRow = table.insertRow(-1);
-    for (let index = 0; index < keyValues.length; index++) {
-        const tableHeader = document.createElement('th');
-        tableHeader.innerHTML = keyValues[index];
-        tableRow.appendChild(tableHeader);
-    }
-    // add JSON data to the table
-    for (let i = 0; i < jsonString.length; i++) {
-        tableRow = table.insertRow(-1);
-        for (let j = 0; j < keyValues.length; j++) {
-            const tabCell = tableRow.insertCell(-1);
-            tabCell.innerHTML = jsonString[i][keyValues[j]];
-        }
-    }
 }
