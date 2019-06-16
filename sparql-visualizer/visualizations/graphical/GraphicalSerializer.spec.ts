45946a9af3db64f9d197b8274e386cdddd0b4fd4
Katharina Müller <katharina.mueller@student.htw-berlin.de>
2019-06-16 18:45:09 +0200

    Adjusted stubbing of GraphSerializer.

--- sparql-visualizer/visualizations/graphical/GraphicalSerializer.spec.ts
+++ sparql-visualizer/visualizations/graphical/GraphicalSerializer.spec.ts
@@ -1,4 +1,4 @@
-import sinon, { SinonStubbedInstance } from 'sinon';
+import sinon from 'sinon';
 import { WikidataEndpoint } from '../../wikidata-endpoint';
 import { composeIFrame, createGraphElement } from './GraphicalSerializer';
 type VisualisationIdentifier = import('../index.types').VisualisationIdentifier;
@@ -9,21 +9,24 @@ describe('Graphical Serializer', () => {
     it('should return of type HTMLElement', () => {
         expect(composeIFrame(ENDPOINT_GRAPH_URL)).toBeInstanceOf(HTMLElement);
     });
+
     it('should return of type IFrameElement', () => {
         expect(composeIFrame(ENDPOINT_GRAPH_URL)).toBeInstanceOf(HTMLIFrameElement);
     });
+
     it('should return a HTMLIFrameElement', () => {
-        const endpoint: SinonStubbedInstance<WikidataEndpoint> = sinon.createStubInstance(
-            WikidataEndpoint
-        );
-        endpoint.getSPARQLVisualisationURL.returns(ENDPOINT_GRAPH_URL);
+        const endpoint: WikidataEndpoint = new WikidataEndpoint({});
+        sinon.stub(endpoint, 'getSPARQLVisualisationURL').returns(ENDPOINT_GRAPH_URL);
 
         const visualisationType: VisualisationIdentifier = 'BubbleChart';
         const stringExample: string = `hello`;
 
-        expect(
-            // tslint:disable-next-line: no-any
-            createGraphElement(stringExample, visualisationType, endpoint as any)
-        ).toBeInstanceOf(HTMLIFrameElement);
+        const graphElement: HTMLElement = createGraphElement(
+            stringExample,
+            visualisationType,
+            endpoint
+        );
+
+        expect(graphElement.matches('iframe')).toBeTruthy();
     });
 });
