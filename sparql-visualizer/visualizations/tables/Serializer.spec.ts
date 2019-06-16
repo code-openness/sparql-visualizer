8630beac754f262a0ce8e422ad5ec846feb87658
unknown <viktoria.sorgalla@fu-berlin.de>
2019-06-16 18:22:43 +0200

    wip

---
+++ sparql-visualizer/visualizations/tables/Serializer.spec.ts
@@ -0,0 +1,15 @@
+describe('Serializer', () => {
+    it('schould create a table element', () => {});
+
+    it('should have a table head in the table', () => {});
+
+    it('should contain only one row in the table head', () => {});
+
+    it('should contain all provided keys in a head', () => {});
+
+    it('should have a table body in the table', () => {});
+
+    it('should contain one or more rows in the table body', () => {});
+
+    it('should contain all provided values in the row', () => {});
+});
