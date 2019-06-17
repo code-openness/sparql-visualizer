ccaa3f921cd572cfe3506775c89d34e6ad670885
unknown <viktoria.sorgalla@fu-berlin.de>
2019-06-17 22:30:43 +0200

    test for tables

--- sparql-visualizer/visualizations/tables/TableCreator.spec.ts
+++ sparql-visualizer/visualizations/tables/TableCreator.spec.ts
@@ -1,11 +1,16 @@
 import sinon from 'sinon';
+import { DataRow } from '../../sparql/index.types';
 import * as SPARQLRequest from '../../sparql/Request';
 import { WikidataEndpoint } from '../../wikidata-endpoint';
 import * as HTMLTable from '../tables/Serializer';
 import { createDataTable } from './TableCreator';
-// import { DataRow } from '../../sparql/index.types';
 
 type SinonStub = import('sinon').SinonStub;
+// const SPARQL_QUERY: string = '# sparql query';
+const DATA_ROW_EXAMPLE: DataRow[] = [
+    { Moep: 'f', Blubb: 'h', kljlk: 'v' },
+    { Moep: 'm', Blubb: 'n', kljlk: 'k' }
+];
 
 describe('TableCreator', () => {
     let endpointStub: WikidataEndpoint;
@@ -23,17 +28,23 @@ describe('TableCreator', () => {
         requestQueryResultsStub.yields();
         const spy = (sinon.spy as unknown) as string;
         await createDataTable(endpointStub, spy);
-        expect(spy).toHaveBeenCalled;
+        expect(spy).toHaveBeenCalledWith;
+        requestQueryResultsStub.restore();
+    });
+
+    it('should call requestQueryResults returns a List', async () => {
+        const requestQueryResultsStub: SinonStub = sinon.stub(SPARQLRequest, 'requestQueryResults');
+        requestQueryResultsStub.yields();
+        const spy = (sinon.spy as unknown) as string;
+        await createDataTable(endpointStub, spy);
+        expect(spy).toContain;
         requestQueryResultsStub.restore();
     });
 
     it('should call crateHTMLTable', async () => {
         const createHTMLTableStub: SinonStub = sinon.stub(HTMLTable, 'createHTMLTable');
         const requestQueryResultsStub: SinonStub = sinon.stub(SPARQLRequest, 'requestQueryResults');
-        requestQueryResultsStub.returns([
-            { Moep: 'f', Blubb: 'h', kljlk: 'v' },
-            { Moep: 'm', Blubb: 'n', kljlk: 'k' }
-        ]);
+        requestQueryResultsStub.returns(DATA_ROW_EXAMPLE);
 
         await createDataTable(endpointStub, '');
 
