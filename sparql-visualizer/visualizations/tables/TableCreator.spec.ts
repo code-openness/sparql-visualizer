c8b81e3de83ebe236b9fefba19d5fab8f9372964
unknown <viktoria.sorgalla@fu-berlin.de>
2019-06-17 21:47:31 +0200

    Test for Table

--- sparql-visualizer/visualizations/tables/TableCreator.spec.ts
+++ sparql-visualizer/visualizations/tables/TableCreator.spec.ts
@@ -22,32 +22,21 @@ describe('TableCreator', () => {
         const requestQueryResultsStub: SinonStub = sinon.stub(SPARQLRequest, 'requestQueryResults');
         requestQueryResultsStub.yields();
         const spy = (sinon.spy as unknown) as string;
-
         await createDataTable(endpointStub, spy);
-
         expect(spy).toHaveBeenCalled;
-
         requestQueryResultsStub.restore();
     });
 
     it('should call crateHTMLTable', async () => {
         const createHTMLTableStub: SinonStub = sinon.stub(HTMLTable, 'createHTMLTable');
-        window.fetch = createHTMLTableStub;
-        createHTMLTableStub.resolves(createStubResponse());
-        // const spy = sinon.spy as unknown as DataRow[];
+        const requestQueryResultsStub: SinonStub = sinon.stub(SPARQLRequest, 'requestQueryResults');
+        requestQueryResultsStub.returns([
+            { Moep: 'f', Blubb: 'h', kljlk: 'v' },
+            { Moep: 'm', Blubb: 'n', kljlk: 'k' }
+        ]);
 
         await createDataTable(endpointStub, '');
 
-        // expect(createHTMLTableStub.calledOnce).toBeTruthy;
-        // expect(spy).toBeCalledWith();
-
-        // createHTMLTableStub.restore();
         sinon.assert.calledOnce(createHTMLTableStub);
-        // expect(createHTMLTableStub.calledOnce).toEqual([endpointStub, '']);
     });
-    function createStubResponse(returnValue: object = {}): Response {
-        return {
-            json: sinon.stub().resolves(returnValue)
-        } as any;
-    }
 });
